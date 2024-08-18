4e2495341e508866d4764eff873f15cdf2e2c866
pruthviarj7714 <marganepruthviraj@gmail.com>
2024-08-18 19:58:27 +0530

    UI fix

--- src/app/(root)/layout.tsx
+++ src/app/(root)/layout.tsx
@@ -1,15 +1,16 @@
-import Sidebar from "../components/Sidebar";
+import Sidebar from "@/components/Sidebar";
+
 
 export default function Layout({ children }: { children: React.ReactNode }) {
   return (
-    <section className="bg-black text-white">
+    <section>
       <div className="flex justify-center h-full">
         {/* Left Sidebar */}
         <div className="fixed left-0 top-0 w-[250px] px-4 hidden lg:block h-full">
           <Sidebar />
         </div>
         {/* Main Content */}
-        <div className="flex-1 max-w-[600px] px-4 h-full lg:ml-[250px] lg:mr-[330px]">
+        <div className="flex-1 max-w-[600px] px-4 h-full lg:ml-[200px] lg:mr-[330px]">
           {children}
         </div>
         {/* Right Sidebar */}
