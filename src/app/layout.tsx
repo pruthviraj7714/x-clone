b9a6a16fa4f6fadf0c933cb9a360ea2993e439f3
pruthviarj7714 <marganepruthviraj@gmail.com>
2024-08-18 19:56:44 +0530

    added provider next-auth

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,7 +2,8 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 import { Toaster } from "@/components/ui/toaster";
-import Providers from "./components/Providers";
+import Providers from "@/components/Providers";
+
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -18,7 +19,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={inter.className}>
+      <body className={`${inter.className} bg-black text-white`}>
         <Providers>
           <Toaster />
           {children}
