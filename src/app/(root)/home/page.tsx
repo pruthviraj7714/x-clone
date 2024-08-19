0734dc837f938b99060492a05eb5f7af80ad50e4
pruthviarj7714 <marganepruthviraj@gmail.com>
2024-08-19 13:58:23 +0530

    added user info route

--- src/app/(root)/home/page.tsx
+++ src/app/(root)/home/page.tsx
@@ -1,6 +1,30 @@
+"use client";
 import Twitte from "@/components/Twitte";
+import { useToast } from "@/components/ui/use-toast";
+import axios from "axios";
+import { useEffect, useState } from "react";
 
 export default function Home() {
+  const [userInfo, setUserInfo] = useState<any>({});
+
+  const { toast } = useToast();
+  const getUserInfo = async () => {
+    try {
+      const res = await axios.get("/api/user/info");
+      setUserInfo(res.data.user);
+      console.log(res.data);
+    } catch (error: any) {
+      toast({
+        title: error.response.data.message,
+        variant: "destructive",
+      });
+    }
+  };
+
+  useEffect(() => {
+    getUserInfo();
+  }, []);
+
   return (
     <div className="border-l border-r p-2 border-gray-400 bg-black text-white flex flex-col">
       <Twitte />
---
+++ src/app/api/user/info/route.ts
@@ -0,0 +1,58 @@
+import authOptions from "@/lib/auth";
+import prisma from "@/lib/db";
+import { getServerSession } from "next-auth";
+import { NextRequest, NextResponse } from "next/server";
+
+export async function GET(req: NextRequest) {
+  const session = await getServerSession(authOptions);
+
+  if (!session || !session.user) {
+    return NextResponse.json(
+      {
+        message: "User Not Found!",
+      },
+      { status: 403 }
+    );
+  }
+
+  try {
+    const user = await prisma.user.findFirst({
+      where: {
+        id: Number(session.user.id),
+      },
+      select: {
+        id: true,
+        username: true,
+        email: true,
+        posts: true,
+        followers: true,
+        followings: true,
+        headerPhoto: true,
+        bio: true,
+        likes: true,
+        bookmarks: true,
+        comments: true,
+        photo: true,
+      },
+    });
+
+    if (!user) {
+      return NextResponse.json(
+        {
+          message: "User Not Found!",
+        },
+        { status: 404 }
+      );
+    }
+
+    return NextResponse.json(user, { status: 200 });
+  } catch (error) {
+    console.error("Error fetching user:", error);
+    return NextResponse.json(
+      {
+        message: "Internal Server Error",
+      },
+      { status: 500 }
+    );
+  }
+}
